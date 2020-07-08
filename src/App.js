741cfdffc00dc5d5b2475a5fc506ce3648ba29b1
Abhishek sah <sah755146@gmail.com>
2020-07-08 18:46:43 +0530

    added react bootsrap inputs and redesign

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import styled from "styled-components";
 import Popup from "reactjs-popup";
 
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav, InputGroup, FormControl, Button } from "react-bootstrap";
 
 function App() {
   const [name, setName] = useState("");
@@ -49,101 +49,88 @@ function App() {
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="mr-auto">
-              <Nav.Link href="#features">Changes</Nav.Link>
-              <Nav.Link href="#pricing">Donate</Nav.Link>
-              {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown> */}
+              <Nav.Link href="#changes">Changes</Nav.Link>
+              <Nav.Link href="#donate">Donate</Nav.Link>
             </Nav>
             <Nav>
-              <Nav.Link href="#deets">More deets</Nav.Link>
+              ` <Nav.Link href="#deets">About</Nav.Link>
               <Nav.Link eventKey={2} href="#memes">
-                Dank memes
+                Contact
               </Nav.Link>
+              `
             </Nav>
           </Navbar.Collapse>
         </Navbar>
       </header>
       <div className="content">
-        According to the researchers at the University of California San Diego's
-        <br></br>
-        school of medicine, one dog year is not equal to seven human
-        <br></br>
-        years. The findings suggest a one-year-old puppy is actually about 30
-        <br></br>
-        in “human years” – an age when humans, at least, might be expected to
-        <br></br>
-        have stopped running riot with the toilet paper.<br></br>
-        <a href="https://www.theguardian.com/lifeandstyle/2020/jul/02/every-human-year-not-equivalent-to-seven-dog-years-scientists-find?utm_campaign=fullarticle&utm_medium=referral&utm_source=inshorts">
-          how we calculate the age
-        </a>
-        <Form onSubmit={doggoToHumanAgeCalc}>
-          {/* <label
-          style={{ color: "#fff", textAlign: "left", justifyContent: "left" }}
-        >
-          Name
-        </label> */}
-          <Input
-            type="text"
-            id="name"
-            placeholder="Dog's Name"
-            value={name}
-            onChange={nameChangeHandler}
-          />
+        <div className="form-area">
+          <Form onSubmit={doggoToHumanAgeCalc}>
+            <InputGroup className="inputfield">
+              <InputGroup.Prepend>
+                <InputGroup.Text id="inputGroup-sizing-sm">
+                  Name
+                </InputGroup.Text>
+              </InputGroup.Prepend>
+              <FormControl
+                aria-label="Name"
+                aria-describedby="inputGroup-sizing-sm"
+                value={name}
+                onChange={nameChangeHandler}
+              />
+            </InputGroup>
+            <InputGroup className="inputfield">
+              <InputGroup.Prepend>
+                <InputGroup.Text id="inputGroup-sizing-sm">Age</InputGroup.Text>
+              </InputGroup.Prepend>
+              <FormControl
+                aria-label="Age"
+                aria-describedby="inputGroup-sizing-sm"
+                value={doggoAge}
+                onChange={ageChangeHandler}
+              />
+            </InputGroup>
 
-          <Input
-            type="text"
-            id="age"
-            placeholder="Dog's Age"
-            value={doggoAge}
-            onChange={ageChangeHandler}
-          />
-          <input
-            // disabled={name && doggoAge}
-            type="submit"
-            value="Find Out"
-            style={{
-              width: 320,
-              height: 50,
-              backgroundColor: "#c2c2c2",
-              color: "#000",
-              // shapeOutside: "none",
-              border: "none",
-              textAlign: "center",
-              outline: "none",
-            }}
-          />
-        </Form>
-        <a
-          class="bmc-button"
-          target="_blank"
-          href="https://www.buymeacoffee.com/abhisheksah"
-        >
-          <img
-            src="https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg"
-            alt="Buy me a Tea"
-          />
-          <span style={{ marginLeft: 5, fontSize: 19 }}>Buy me a Tea</span>
-        </a>
-        <MyPopup
-          open={showPopup}
-          closeOnDocumentClick
-          onClose={() => setShowPopup(false)}
-        >
-          <h3 style={{ color: "#000" }}>
-            Your {name} is {Math.floor(humanAge)} years old
-          </h3>
-        </MyPopup>
+            <Button className="submit-btn" type="submit">
+              Find Out
+            </Button>
+          </Form>
+
+          {/* <a
+            class="bmc-button"
+            target="_blank"
+            href="https://www.buymeacoffee.com/abhisheksah"
+          >
+            <img
+              src="https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg"
+              alt="Buy me a Tea"
+            />
+            <span style={{ marginLeft: 5, fontSize: 19 }}>Buy me a Tea</span>
+          </a> */}
+          <MyPopup
+            open={showPopup}
+            closeOnDocumentClick
+            onClose={() => setShowPopup(false)}
+          >
+            <h3 style={{ color: "#000" }}>
+              Your {name} is {Math.floor(humanAge)} years old
+            </h3>
+          </MyPopup>
+        </div>
+        <div className="info-area">
+          According to the researchers at the University of California San
+          Diego's
+          <br></br>
+          school of medicine, one dog year is not equal to seven human
+          <br></br>
+          years. The findings suggest a one-year-old puppy is actually about 30
+          <br></br>
+          in “human years” – an age when humans, at least, might be expected to
+          <br></br>
+          have stopped running riot with the toilet paper.<br></br>
+          <a href="https://www.theguardian.com/lifeandstyle/2020/jul/02/every-human-year-not-equivalent-to-seven-dog-years-scientists-find?utm_campaign=fullarticle&utm_medium=referral&utm_source=inshorts">
+            how we calculate the age
+          </a>
+        </div>
       </div>
     </div>
   );
@@ -170,21 +157,10 @@ const Form = styled.form`
   display: flex;
   align-items: center;
   margin-top: 1em;
-  background: #000;
+  background: #343a41;
   width: 400px;
   height: 300px;
   justify-content: center;
-`;
-
-const Input = styled.input`
-  width: 300px;
-  height: 35px;
-  padding: 10px;
-  margin-bottom: 25px;
-  border: none;
-  border-radius: 1px;
-  text-align: center;
-  outline: none;
 `;
 
 export default App;
